8444c6ae2450640eb18ee64cb6783de35f31e16a
Jozsi <anderkajozsef@gmail.com>
2020-07-03 19:05:10 +0200

    frissit

--- js/hofok.js
+++ js/hofok.js
@@ -1,16 +1,80 @@
-let napiHomerseklet = [18, 19, 23, 25, 28, 16, 10];
-//let kivalasztottNap = document.querySelector("select[name='select1']");
+let napiHomerseklet = [-2, 19, 23, 25, 28, 16, 10];
+//let nap = document.querySelector("select[name='select1']");
+let nap = 4;
+hanyfoklesz(nap);
+maximum();
+minimum();
+atlag();
 
 function hanyfoklesz(nap) {
-  //  let eredmeny = napiHomerseklet[nap];
-  //  print(eredmeny);
-  print(nap)  ;
+  let eredmeny = napiHomerseklet[nap];
+  printNapiHofok(eredmeny);
+  miAzAjanlat(eredmeny);
 }
 
+function miAzAjanlat(eredmeny) {
+  let napiAjanlatEredmeny;
+  if (eredmeny < 0) {napiAjanlatEredmeny = "Forró csoki"
+}   
+else if (eredmeny >= 0 && eredmeny < 15) {napiAjanlatEredmeny = "Meleg tea"
+}
+else if (eredmeny >= 15 && eredmeny < 25) {napiAjanlatEredmeny = "Finom süti"
+}
+else if (eredmeny >= 25) {napiAjanlatEredmeny = "Jéghideg limonádé"
+}
+printNapiAjanlatEredmeny(napiAjanlatEredmeny);
+}  
 
+function maximum() {
+  let max = napiHomerseklet[0];
+  for (let i = 0; i < napiHomerseklet.length; i++) {
+    if (napiHomerseklet[i] > max) {
+      max = napiHomerseklet[i];
+    }
+    printMax(max);
+  }
+}
 
+function minimum() {
+  let min = napiHomerseklet[0];
+  for (let i = 0; i < napiHomerseklet.length; i++) {
+    if (napiHomerseklet[i] < min) {
+      min = napiHomerseklet[i];
+    }
+    printMin(min);
+  }
+}
 
-function print(eredmeny) {
-    let hofok = document.querySelector("span.hofok");
-    hofok.innerHTML = eredmeny;
+function atlag() {
+  let atl = 0;
+  for (let i = 0; i < napiHomerseklet.length; i++) {
+    atl = atl + napiHomerseklet[i];
+  }
+  atl = atl / napiHomerseklet.length;
+  printAtl(atl);
+}
+
+function printNapiHofok(eredmeny) {
+  let hofok = document.querySelector("span.hofok");
+  hofok.innerHTML = eredmeny;
+}
+
+function printNapiAjanlatEredmeny(napiAjanlatEredmeny) {
+  let napiAjanlat = document.querySelector("span.ajanlat");
+  napiAjanlat.innerHTML = napiAjanlatEredmeny;
+}
+
+function printMax(max) {
+  let maximum = document.querySelector("span.maximum");
+  maximum.innerHTML = max;
+}
+
+function printMin(min) {
+  let minimum = document.querySelector("span.minimum");
+  minimum.innerHTML = min;
+}
+
+function printAtl(atl) {
+  let atlag = document.querySelector("span.atlag");
+  atlag.innerHTML = atl;
 }
